268deea9beb64ccc5de12bee792dd938fb1de3c7
Andreia Vieira <andreiavieira2353@gmail.com>
2022-11-23 20:43:28 -0300

    export db

--- src/database/db.js
+++ src/database/db.js
@@ -5,8 +5,15 @@ dotenv.config();
 const mongoClient = new MongoClient(process.env.MONGO_URI)
 
 try{
-await mongoClient.connect();
-console.log("MongoDB Connected!");
+await mongoClient.connect()
+console.log("MongoDB Connected!")
+
 }catch (err) {
     console.log(err)
 }
+
+const db = mongoClient.db("PorAiStore");
+export const usersCollection = db.collection("users");
+export const sessionsCollection = db.collection("sessions");
+
+export default mongoClient;
