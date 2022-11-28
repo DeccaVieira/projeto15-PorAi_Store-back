3da44cb9a900e379e19f95b7fd30c0d7a1654f32
Andreia Vieira <andreiavieira2353@gmail.com>
2022-11-28 16:54:09 -0300

    fix delete

--- src/controllers/checkout.controller.js
+++ src/controllers/checkout.controller.js
@@ -1,30 +1,31 @@
 import { finalizeCollection, shoppingCartCollection } from "../database/db.js";
 
 export async function createCheckout(req, res) {
-    
   const user = res.locals.user;
   console.log(user);
   try {
-    const carts = await shoppingCartCollection.find({ user: user._id }).toArray();
-    const cartsWithNold = carts.map(({user, price, from, to, quantity}) => {
-        return {user, price, from, to, quantity}
-    })
+    const carts = await shoppingCartCollection
+      .find({ user: user._id })
+      .toArray();
+    const cartsWithNold = carts.map(({ user, price, from, to, quantity }) => {
+      return { user, price, from, to, quantity };
+    });
     await finalizeCollection.insertMany(cartsWithNold);
-await shoppingCartCollection.deleteMany({user:user._Id})
-res.sendStatus(200);
-} catch (err) {
-  console.log(err);
-  res.sendStatus(500);
-}
+    await shoppingCartCollection.deleteMany({ user: user._id });
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
 export async function showCheckout(req, res) {
   const user = res.locals.user;
-  
-  try {
-    const purchases = await finalizeCollection.find({ user: user._id }).toArray();
-  res.send(purchases)
-    
 
+  try {
+    const purchases = await finalizeCollection
+      .find({ user: user._id })
+      .toArray();
+    res.send(purchases);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
